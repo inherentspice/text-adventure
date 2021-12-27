8929345d71f20ac57547ce972b81c0f176cac224
Stephen Smith <stephenlehanesmith@gmail.com>
2021-12-27 17:18:31 +0800

    defined function that uses CountVectoriser to get top n words, cleaned up unused imports, added guiding comments.

--- project.py
+++ project.py
@@ -1,24 +1,39 @@
 # import libraries
 
-from collections import Counter
 from sklearn.feature_extraction.text import CountVectorizer
-from nltk.stem import PorterStemmer
-from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
-from nltk.corpus import wordnet, stopwords
+from nltk.corpus import stopwords
 
-# tokenize web-scraping text
-text = 'On June 1, 1865, Senator Charles Sumner referred to the most famous speech ever given by President Abraham Lincoln. In his eulogy on the slain president'
-tokenized = word_tokenize(text)
+# load text that you want to count word frequencies
+text = ''
 
-# remove stop words
-stop_words = set(stopwords.words('english'))
-filtered_sentence = []
+# function that tokenizes, removes stop words, vectorizers, and counts the most common words
+def get_top_n_words(text, n=None):
+    """
+    List the top n words in a vocabulary according to occurrence in a text without very common words.
 
-for t in tokenized:
-    if t not in stop_words:
-        filtered_sentence.append(t)
+    get_top_n_words('fish, flew dangerously far toward the flying fly for only the
+    fly near a flying fish is truly a fly, 2')
+    -->
+    [('fly', 3),
+    ('fish', 2)]
+    """
+    tokenized = word_tokenize(text)
+    stop_words = set(stopwords.words('english'))
+    filtered_sentence = []
 
-print(tokenized)
-print(filtered_sentence)
+    for t in tokenized:
+        if t not in stop_words:
+            filtered_sentence.append(t)
+    vec = CountVectorizer().fit(filtered_sentence)
+    bag_of_words = vec.transform(filtered_sentence)
+    sum_words = bag_of_words.sum(axis=0)
+    words_freq = [(word, sum_words[0, idx]) for word, idx in vec.vocabulary_.items()]
+    words_freq = sorted(words_freq, key=lambda x: x[1], reverse=True)
+    return words_freq[:n]
 
+# call function
+# change number to however many words you want to see
+common_words = get_top_n_words(text, 20)
+for word, freq in common_words:
+    print(word, freq)
