f05582c2044508254499faa99b19b69e5037f33d
Stephen Smith <stephenlehanesmith@gmail.com>
2021-12-30 09:50:41 +0800

    preproccessed csv file so it could be read by the get_top_n_wods function

--- project.py
+++ project.py
@@ -9,7 +9,7 @@ webpage = webpage_response.content
 soup = BeautifulSoup(webpage,'html.parser')
 
 spans = set(soup.find_all('span', attrs={'class':"label-text"}))
-    
+
 with open("five_options_questions.csv", "w") as external_file:
     for span in spans:
         if span.get_text("|").split("|")[:-1] != ['E-mail '] and span.get_text("|").split("|")[:-1] != ['Password ']:
@@ -53,42 +53,46 @@ from nltk.corpus import stopwords
 import csv
 
 #preprocess csv
-text = []
+rows = []
 with open('five_options_questions.csv', 'r') as file:
     csvreader = csv.reader(file)
     for row in csvreader:
-        text.append(row)
-
-# load text that you want to count word frequencies
+        rows.append(row)
 
 
-# # function that tokenizes, removes stop words, vectorizers, and counts the most common words
-# def get_top_n_words(text, n=None):
-#     """
-#     List the top n words in a vocabulary according to occurrence in a text without very common words.
-#
-#     get_top_n_words('fish, flew dangerously far toward the flying fly for only the
-#     fly near a flying fish is truly a fly, 2')
-#     -->
-#     [('fly', 3),
-#     ('fish', 2)]
-#     """
-#     tokenized = word_tokenize(text)
-#     stop_words = set(stopwords.words('english'))
-#     filtered_sentence = []
-#
-#     for t in tokenized:
-#         if t not in stop_words:
-#             filtered_sentence.append(t)
-#     vec = CountVectorizer().fit(filtered_sentence)
-#     bag_of_words = vec.transform(filtered_sentence)
-#     sum_words = bag_of_words.sum(axis=0)
-#     words_freq = [(word, sum_words[0, idx]) for word, idx in vec.vocabulary_.items()]
-#     words_freq = sorted(words_freq, key=lambda x: x[1], reverse=True)
-#     return words_freq[:n]
-#
-# # call function
-# # change number to however many words you want to see
-# common_words = get_top_n_words(text, 20)
-# for word, freq in common_words:
-#     print(word, freq)
+#load text that you want to count word frequencies
+text = ''
+for i in rows:
+    for char in i:
+        text += char
+
+# function that tokenizes, removes stop words, vectorizers, and counts the most common words
+def get_top_n_words(text, n=None):
+    """
+    List the top n words in a vocabulary according to occurrence in a text without very common words.
+
+    get_top_n_words('fish, flew dangerously far toward the flying fly for only the
+    fly near a flying fish is truly a fly, 2')
+    -->
+    [('fly', 3),
+    ('fish', 2)]
+    """
+    tokenized = word_tokenize(text)
+    stop_words = set(stopwords.words('english'))
+    filtered_sentence = []
+
+    for t in tokenized:
+        if t not in stop_words:
+            filtered_sentence.append(t)
+    vec = CountVectorizer().fit(filtered_sentence)
+    bag_of_words = vec.transform(filtered_sentence)
+    sum_words = bag_of_words.sum(axis=0)
+    words_freq = [(word, sum_words[0, idx]) for word, idx in vec.vocabulary_.items()]
+    words_freq = sorted(words_freq, key=lambda x: x[1], reverse=True)
+    return words_freq[:n]
+
+# call function
+# change number to however many words you want to see
+common_words = get_top_n_words(text, 20)
+for word, freq in common_words:
+    print(word, freq)
