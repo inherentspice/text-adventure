9e084ab442bf6a33726676cc9718f490d0eeb12e
Stephen Smith <stephenlehanesmith@gmail.com>
2021-12-31 11:50:15 +0800

    added story lines, and nested if/else statements

--- Story.py
+++ Story.py
@@ -3,13 +3,24 @@
 # [time 5] [would 5] [life 5] [things 4] [avoid 4] [work 4] [think 3]
 # Now use these as basis for text-adventure
 
-beginning = input("Would you like to explore the inner reaches of your mind?")
+beginning = input("Would you like to explore the inner reaches of your mind? (yes/no)")
 if beginning == 'Yes'.lower().strip():
-    print('k')
+    two = input('In front of you are doors to two rooms. The room on the left is full of chatting people. The room on your right is empty and has food and drinks, and a library of for you to read. Which way do you go? (left/right)')
+    if two == "Left".lower().strip():
+        three = input("The faces of the people shift and shimmer in the light. You recognize them. Work friends. Do you discuss managements' new policy on water bottles in the work space or the weather? (water/weather)")
+        if three == "water".lower().strip():
+            print('k')
+        else:
+            print('jk')
+    elif two == "Right".lower().strip():
+        three_a = input("You sit in a comfy chair. Pick up a book by Stephen Hawking. As you crack the spine, a voice whispers to you, 'Come over to the mirror.' What do you do? (ignore/follow)")
+        if three_a == 'ignore'.lower().strip():
+            print('m')
+        else:
+            print('ol')
+    else:
+        print('sorry')
 else:
     print('You coward. Come back later.')
 
-'''
-1. In front of you are doors to two rooms. The room on the left is full of people. They seem to be enjoying themselves, talking and laughing. The room on your right has no one else in it, but it is cozy and nicely decorated. There are plenty of food and drinks you can help yourself to and some books for you to read. Which way do you go?
-2. 
-'''
+
